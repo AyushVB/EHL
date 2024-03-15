bb3dd8f4c31799b3aa127340fd4da39140e95832
Ayush Bendale <96472762+AyushVB@users.noreply.github.com>
2024-03-15 23:39:19 +0530

    minor changes in connectDB file

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async (DB_URL) => {
-  const DB_OPTIONS = {
-    useNewUrlParser: true,
-  };
   mongoose.set("strictQuery", false);
   try {
-    await mongoose.connect(DB_URL, DB_OPTIONS);
+    await mongoose.connect(DB_URL);
     console.log("connected successfully on mongoDB Atlas ....");
   } catch (error) {
     console.log(`error: ${error}`);
